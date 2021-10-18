e63b6ea940db1f9287b8b097ad7e61b49513eba7
Galileu <giocondo.g@gmail.com>
2021-10-18 07:46:58 -0300

    [ADD] - frontend

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,46 @@
+import { Container } from "./styles";
+import { useTransactions } from "../../hooks/useTransactions";
+import { formatCurrency } from "../../Formaters";
+
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  amount: number;
+  category: string;
+  createdAt: string;
+}
+
+export function TransactionsTable() {
+  const { transactions } = useTransactions();
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction: Transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {formatCurrency(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
+}
